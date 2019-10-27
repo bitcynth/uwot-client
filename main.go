c0780964cb104446c5e90d9d6f9445e01c1f657c
Cynthia Revstrom <me@cynthia.re>
2019-10-27 14:23:47 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var rootServer = flag.String("server", "uwot.cynthia.re:43443", "uwot server")
+
+func main() {
+	flag.Parse()
+
+	conf := &tls.Config{}
+
+	conn, err := tls.Dial("tcp", *rootServer, conf)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer conn.Close()
+
+	n, err := conn.Write([]byte(os.Args[1] + "\r\n"))
+	if err != nil {
+		log.Fatal(n, err)
+		return
+	}
+
+	resp := ""
+	buf := make([]byte, 512)
+	for {
+		n, err := conn.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		resp += string(buf[:n])
+	}
+
+	fmt.Println(resp)
+}
