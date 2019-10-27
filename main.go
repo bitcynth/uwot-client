c34d3d4afd7ff6b5a5e7a203f61ee4a6a01f06b1
Cynthia Revstrom <me@cynthia.re>
2019-10-27 17:10:51 +0100

    add EV stuff :P

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/asn1"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"strconv"
+	"strings"
 )
 
+// Trusted EV OIDs, to request an OID to be added, contact: me@cynthia.re
+var evOids = []asn1.ObjectIdentifier{
+	getOid("2.16.840.1.114412.2.1"),
+	getOid("1.3.6.1.4.1.6334.1.100.1"),
+	getOid("2.16.840.1.113733.1.7.23.6"),
+}
+
 var rootServer = flag.String("server", "uwot.cynthia.re:43443", "uwot server")
+
+func getOid(strrep string) asn1.ObjectIdentifier {
+	parts := strings.Split(strrep, ".")
+	parts2 := make([]int, len(parts))
+	for i, part := range parts {
+		parts2[i], _ = strconv.Atoi(part)
+	}
+	return parts2
+}
 
 func main() {
 	flag.Parse()
@@ -22,6 +42,22 @@ func main() {
 	}
 
 	defer conn.Close()
+
+	isEV := false
+	state := conn.ConnectionState()
+	var cert *x509.Certificate
+	for _, v := range state.PeerCertificates {
+		if !v.IsCA {
+			cert = v
+		}
+	}
+	for _, v := range cert.PolicyIdentifiers {
+		for _, oid := range evOids {
+			if v.Equal(oid) {
+				isEV = true
+			}
+		}
+	}
 
 	n, err := conn.Write([]byte(flag.Arg(0) + "\r\n"))
 	if err != nil {
@@ -44,4 +80,8 @@ func main() {
 	}
 
 	fmt.Println(resp)
+
+	if isEV && len(cert.Subject.Organization) > 0 && len(cert.Subject.Country) > 0 {
+		fmt.Printf("\033[32m\033[5mVerified by: %s (%s)\033[0m\n", cert.Subject.Organization[0], cert.Subject.Country[0])
+	}
 }
